631b92f67be27daef40a6b3786e166e0d73b3e8e
manikandandevops <71846327+manikandandevops@users.noreply.github.com>
2020-11-23 18:40:34 +0530

    test

--- main.go
+++ main.go
@@ -4,52 +4,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"sort"
 	"strings"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		f := fib()
-
-		res := &response{Message: "Hello World"}
-
-		for _, e := range os.Environ() {
-			pair := strings.Split(e, "=")
-			res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
-		}
-		sort.Strings(res.EnvVars)
-
-		for i := 1; i <= 90; i++ {
-			res.Fib = append(res.Fib, f())
-		}
-
-		// Beautify the JSON output
-		out, _ := json.MarshalIndent(res, "", "  ")
-
-		// Normally this would be application/json, but we don't want to prompt downloads
-		w.Header().Set("Content-Type", "text/plain")
-
-		io.WriteString(w, string(out))
-
-		fmt.Println("Hello world - the log message")
-	})
-	http.ListenAndServe(":8080", nil)
-}
-
 type response struct {
 	Message string   `json:"message"`
 	EnvVars []string `json:"env"`
-	Fib     []int    `json:"fib"`
 }
 
-func fib() func() int {
-	a, b := 0, 1
-	return func() int {
-		a, b = b, a+b
-		return a
+func hello() string {
+	var helloInput string = "CI/CD Workshop today with everyone"
+	return helloInput
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hi there, I love %s!", hello())
+
+	res := &response{Message: hello()}
+
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
 	}
+	sort.Strings(res.EnvVars)
+
+	// Beautify the JSON output
+	out, _ := json.MarshalIndent(res, "", "  ")
+
+	// Normally this would be application/json, but we don't want to prompt downloads
+	w.Header().Set("Content-Type", "text/plain")
+
+	io.WriteString(w, string(out))
+
+	fmt.Println("Http Call")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
